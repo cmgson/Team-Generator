b0d54191ca687071a0ae2d890bb4042806a7903a
Chris Gutierrez <cmgson1@gmail.com>
2021-02-19 13:04:02 -0600

    added file paths for exports, classes, and subclasses

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
+const Employee = require('Develop/lib/Employee.js');
+
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(github) {
+        super(name, id, email);
+        this.github = github;
+
+    }
+    getRole() {
+        return 'Engineer';
+    }
+
+    getGithub() {};
+}
+
+module.exports = Engineer;
